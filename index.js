b6b766d12a2606a80302325c4a6b5486811a1fb5
Jan Bölsche <regular.gonzales@googlemail.com>
2017-07-05 17:38:21 +0200

    Use pull stream to filter and transform changes stream

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var ChangesStream = require('changes-stream')
 var through2 = require('through2')
 var pump = require('pump')
 var path = require('path')
+var pull = require('pull-stream')
 var Pushable = require('pull-pushable')
 
 module.exports = function getStream(seq) {
@@ -14,9 +15,42 @@ module.exports = function getStream(seq) {
   var pushable = Pushable(true, function(err) {
     running = false
   })
-  
   run()
-  return pushable.source
+
+  return pull(
+    pushable.source,
+    pull.filter( (change)=>{
+      if (!change.doc) {
+        console.error('skipping %s - invalid document (seq: %s)', change.id, change.seq)
+        return false
+      }
+      return true
+    }),
+    pull.through( (change)=>{
+      clean(change.doc)
+    }),
+    pull.filter( (change)=>{
+      var doc = change.doc
+      if (!doc.versions || doc.versions.length === 0) {
+        console.error('skipping %s - no versions detected (seq: %s)', change.id, change.seq)
+        return false
+      }
+      return true
+    }),
+    pull.map( (change)=>{
+      var doc = change.doc
+      var modified = doc && doc.time && doc.time.modified
+      return Object.keys(doc.versions).map( (version)=>{
+        var pkg = doc.versions[version]
+        pkg._seq = change.seq
+        pkg._modified = modified
+        pkg.id = change.id + '@' + version
+        return pkg
+      })
+    }),
+    pull.flatten(),
+    pull.filter()
+  )
 
   function run () {
     console.log('feching changes since sequence #%s', seq)
@@ -35,32 +69,8 @@ module.exports = function getStream(seq) {
   }
 
   function transform (change, env, cb) {
-    var doc = change.doc
-
-    clean(doc)
-
-    var modified = doc && doc.time && doc.time.modified
-    var seq = change.seq
-
-    if (!doc) {
-      console.log('skipping %s - invalid document (seq: %s)', change.id, seq)
-      return cb()
-    } else if (!doc.versions || doc.versions.length === 0) {
-      console.log('skipping %s - no versions detected (seq: %s, modified: %s)', change.id, seq, modified)
-      return cb()
-    }
-
-    var versions = Object.keys(doc.versions)
-    while(versions.length) {
-      var version = versions.shift()
-      if (version) {
-        var pkg = doc.versions[version]
-        pkg._seq = seq
-        pkg.id = change.id + '@' + version
-        pushable.push(pkg)
-      }
-    }
-    cb()
+    pushable.push(change)
+    cb(running ? null : new Error('Stream aborted'))
   }
 
 }
